21a5e00435bb0d4e167ff5217b5e48eaaff073a4
Danny DOC <danchibu@outlook.com>
2021-02-07 18:03:26 +0100

    The Finish Day 07/02/2021 I'm so very happy

---
+++ Hacker1/app.py
@@ -0,0 +1,4 @@
+with open("Code.html.html", "a") as f:
+    read_data = f.write("<p>Hello world</p>")
+
+print(f.write("<h1>Fuck this shit</p>"))
---
+++ Hacker2/app2.py
@@ -0,0 +1,8 @@
+print("This program returns the letter V an approximate 10 times")
+
+v = 1
+t = 0
+while v <= 10:
+    t += 1
+    v += 1
+    print(f"{t}.\tV")
